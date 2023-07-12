b4491dca68b788f658efec4cb59d99607da06348
nv <naverstay.me@gmail.com>
2023-07-12 20:13:23 +0200

    persist

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
-import {store} from "./store/store";
+import {persistor, store} from "./store/store";
 import {Provider} from "react-redux";
+import {PersistGate} from "redux-persist/integration/react";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,9 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {setupListeners} from "@reduxjs/toolkit/query";
 import {createLogger} from "redux-logger/src";
+import storage from "redux-persist/lib/storage";
+import {persistReducer, persistStore} from "redux-persist";
+import storageSession from "reduxjs-toolkit-persist/lib/storage/session";
 import {apiSlice} from "../features/api/apiSlice";
 import {interfaceSlice} from "../slices/interfaceSlice";
 import {cartSlice} from "../slices/cartSlice";
@@ -9,17 +12,26 @@ const logger = createLogger({
   collapsed: true
 });
 
+const persistConfig = {
+  key: "garden-root",
+  storage
+};
+
 const reducers = combineReducers({
   api: apiSlice.reducer,
   interface: interfaceSlice.reducer,
   cart: cartSlice.reducer
 });
 
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer: reducers,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware)
-      //.concat(logger)
+  //.concat(logger)
 });
 
 setupListeners(store.dispatch);
+
+export const persistor = persistStore(store);
